d30a196f24149b46701beb66d609d5a7c656091e
Ben Morris <ben@bendmorris.com>
2014-08-28 00:01:11 -0700

    Niche partitioning. As richness goes up, this should get more forgiving to make things more manageable.

--- src/Species.hx
+++ src/Species.hx
@@ -207,7 +207,7 @@ class Species
 			}
 			for (sp in typedSpecies)
 			{
-				abundances[sp] -= needed * species[sp].value * abundances[sp] / totalAmt * HXP.elapsed / time / 1.25;
+				abundances[sp] -= needed * species[sp].value * abundances[sp] / totalAmt * HXP.elapsed / time / (1 + richness / 40);
 				if (!diets[name].exists(sp)) diets[name][sp] = 0;
 				diets[name][sp] += species[sp].value * abundances[sp] * cost.value;
 			}
